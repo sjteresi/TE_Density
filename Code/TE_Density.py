f0f01b03fe586fead0ddcca73682b98306346207
Scott T <teresiscott@gmail.com>
2019-07-28 22:09:19 -0400

    Remove null objects

--- Code/TE_Density.py
+++ Code/TE_Density.py
@@ -1,12 +1,12 @@
 # By Scott Teresi
 #-------------------------------------------
 # Imports
+
 import numpy as np
 import pandas as pd
 import os
 #-------------------------------------------
-
-# Helper functions
+# Directory Movement and Helpers 
 
 def cwd():
     """ Prints the current working directory """
@@ -26,12 +26,14 @@ def ch_main_path():
     os.chdir('/home/scott/Documents/Uni/Research/Projects/TE_Density/Code/')
 
 def get_head(Data):
+    """ Get the heading of the Pandaframe """
     try:
         print(Data.head())
     except AttributeError as e:
         raise AttributeError('Your input data was incorrect, check to make sure it is a Pandaframe')
 
 def save_output(Data, Output_Name):
+    """ Save the output of the Pandaframe """
     ch_output_data_path() # change to output directory
     Data.to_csv(Output_Name,
             header=True,
@@ -39,65 +41,91 @@ def save_output(Data, Output_Name):
     ch_main_path() # change to Code directory
 
 
+#-------------------------------------------
+# PandaFrame Helper Functions
 
-def import_data():
-    my_data = 'camarosa_gtf_data.gtf'
+def get_dtypes(my_df):
+    print(TE_Data.dtypes)
+
+def get_nulls(my_df):
+    null_columns = my_df.columns[my_df.isnull().any()]
+    count_of_null = my_df[null_columns].isnull().sum()
+    print('Counts of null values per column: ' '\n', count_of_null, '\n')
+
+    rows_where_null = my_df[my_df.isnull().any(axis=1)][null_columns].head()
+    print('Rows where null exist: ', '\n', rows_where_null, '\n')
+
+def drop_nulls(my_df):
+    print('DROPPING ROWS WITH AT LEAST ONE NULL VALUE!!!')
+    my_df = my_df.dropna(axis = 0, how ='any')
+    return my_df
+
+
+#-------------------------------------------
+# Main Functions
+
+def import_genes():
+    """ Import Genes File """
+    my_data = 'camarosa_gtf_data.gtf' # DECLARE YOUR DATA NAME 
     ch_input_data_path()
 
-    col_names = ["Chromosome",
-            'Software',
-            'Feature',
-            'Start',
-            'Stop',
-            'Nonsense1',
-            'Nonsense2',
-            'Nonsense3',
-            'FullName']
+    col_names = ["Chromosome", 'Software', 'Feature', 'Start', 'Stop', \
+        'Score', 'Strand', 'Frame', 'FullName']
 
-    col_to_use = ["Chromosome",
-            'Software',
-            'Feature',
-            'Start',
-            'Stop',
-            'FullName']
+    col_to_use = ["Chromosome", 'Software', 'Feature', 'Start', 'Stop', \
+        'FullName']
 
     Gene_Data = pd.read_csv(my_data,
             sep='\t+',
             header=None,
             engine='python',
             names = col_names,
-            usecols = col_to_use,)
+            usecols = col_to_use)
 
 
-    Gene_Data = Gene_Data[Gene_Data.Feature == 'gene'] # drop non-gene rows
-    Gene_Data[['Name1', 'Gene_Name']] = Gene_Data.FullName.str.split(';Name=', expand=True) # first step to fix names
-    Gene_Data = Gene_Data.set_index('Gene_Name') # set new names as index
-    Gene_Data = Gene_Data.drop(['FullName', 'Name1'], axis = 1) # remove extraneous rows
+    Gene_Data = Gene_Data[Gene_Data.Feature == 'gene']
+        # drop non-gene rows
+
+    Gene_Data[['Name1', 'Gene_Name']] = Gene_Data.FullName.str.split(';Name=', expand=True)
+        # first step to fix names
+
+    Gene_Data = Gene_Data.set_index('Gene_Name')
+        # set new names as index
+
+    Gene_Data = Gene_Data.drop(['FullName', 'Name1'], axis = 1)
+        # remove extraneous rows
+
+    return Gene_Data
 
 
+def import_transposons():
+    """ Import the TEs """
+    my_data = 'camarosa_gff_data.gff' # DECLARE YOUR DATA NAME 
+    ch_input_data_path()
 
+    col_names = ["Chromosome", 'Software', 'Feature', 'Start', 'Stop', \
+        'Score', 'Strand', 'Frame', 'Attribute']
 
-    get_head(Gene_Data)
+    col_to_use = ["Chromosome", 'Software', 'Feature', 'Start', 'Stop']
 
+    TE_Data = pd.read_csv(my_data,
+            sep='\t+',
+            header=None,
+            engine='python',
+            names = col_names,
+            usecols = col_to_use)
+            #dtype = {'Start':int})
 
+    TE_Data[['Family', 'Subfamily']] = TE_Data.Feature.str.split('/', expand=True)
+        # step to fix TE names
 
+    TE_Data = TE_Data.drop('Feature', axis=1)
+    TE_Data.Subfamily = TE_Data.Subfamily.astype(str)
+        # Some of the subfamilies are None, needs to be string
 
-
-
-
-
-
-
-    save_output(Gene_Data, 'test.csv')
-
-
-
-
-
-
-
-
-
+    TE_Data = drop_nulls(TE_Data) # Dropping nulls because I checked
+    TE_Data.Start = TE_Data.Start.astype(int) # Converting to int for space
+    TE_Data.Stop = TE_Data.Stop.astype(int) # Converting to int for space
 
 
 
@@ -105,10 +133,10 @@ def import_data():
 
 
 if __name__ == '__main__':
-    import_data()
-
-
-
-    #print(list(Gene_Data.columns))
-
+    #Gene_Data = import_genes()
     #get_head(Gene_Data)
+    import_transposons()
+    
+
+
+
