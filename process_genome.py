016d3e5bb1c3015b7aac372d525d271a0bac611a
Scott Teresi <teresiscott@gmail.com>
2020-08-31 17:13:08 -0400

    Update argparse to be more descriptive

--- process_genome.py
+++ process_genome.py
@@ -69,7 +69,8 @@ if __name__ == "__main__":
         "tes_input_file", type=str, help="parent path of transposon file"
     )
     parser.add_argument(
-        "genome_id", type=str, help="string of the genome to be run, for clarity"
+        "genome_id", type=str, help="string of the genome to be run, for
+        clarity and naming conventions."
     )
     parser.add_argument(
         "--config_file",
@@ -86,9 +87,24 @@ if __name__ == "__main__":
         help="parent directory to output overlap data",
     )
 
-    parser.add_argument("--reset_h5", action="store_true")
-    parser.add_argument("--contig_del", action="store_false")
-    parser.add_argument("--revise_anno", action="store_true")
+    parser.add_argument("--reset_h5", action="store_true", help="Forces the
+                        recreation of the h5 cached files for the gene and TE
+                        annotations. Desirable if you have previously run the
+                        pipeline and you modified your annotation files for a
+                        second run-through. This is especially useful if
+                        you have modified the input gene or TE annotation but have not
+                        changed the filenames.")
+    parser.add_argument("--contig_del", action="store_false", help="Deletes
+                        entries (rows) in the gene annotation and TE annotation
+                        files that are labelled with any variation of contig*
+                        in the chromosome field (case insensitive).")
+    parser.add_argument("--revise_anno", action="store_true", help="Forces the
+                        recreation of a revised TE annotation file. Desirable if
+                        you have previously created a revised TE annotation but
+                        you want the pipeline to create a new one from scratch
+                        and overwrite the cache. This is especially useful if
+                        you have modified the input TE annotation but have not
+                        changed the filename.")
 
     parser.add_argument(
         "--output_dir",
@@ -108,7 +124,6 @@ if __name__ == "__main__":
     args.overlap_dir = os.path.abspath(args.overlap_dir)
     args.output_dir = os.path.abspath(args.output_dir)
 
-    # NOTE this is the new location
     filtered_input_data_loc = os.path.abspath(
         os.path.join(args.output_dir, "filtered_input_data")
     )
@@ -116,7 +131,6 @@ if __name__ == "__main__":
         os.path.join(args.output_dir, filtered_input_data_loc, "input_h5_cache")
     )
 
-    # NOTE I do not see this being used
     revised_input_data_loc = os.path.abspath(
         os.path.join(args.output_dir, filtered_input_data_loc, "revised_input_data")
     )
