b466f1ea2e433d8f8a7f9724d9e745f285482fe0
Scott Teresi <teresiscott@gmail.com>
2021-06-09 13:00:18 -0400

    Add code to create Human dataset example, add a simple diagnostic tool to Density_Data as well as a method for removing the extraneous revision sets

--- examples/Human/src/sync_remote_to_local.sh
+++ examples/Human/src/sync_remote_to_local.sh
@@ -1,1 +1,1 @@
-rsync -ave ssh teresisc@rsync.hpcc.msu.edu:/mnt/research/edgerpat_lab/Scotty/TE_Density_Example_Data/Rice/ /home/scott/Documents/Uni/Research/Projects/TE_Density_Example_Data/Rice/
+rsync -ave ssh teresisc@rsync.hpcc.msu.edu:/mnt/research/edgerpat_lab/Scotty/TE_Density_Example_Data/Human/ /home/scott/Documents/Uni/Research/Projects/TE_Density_Example_Data/Human/
