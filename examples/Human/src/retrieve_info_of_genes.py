cdba845274e4553e31b5332ca1d33d9e2bc0297b
Scott Teresi <teresiscott@gmail.com>
2021-06-09 13:00:18 -0400

    Add code to create Human dataset example, add a simple diagnostic tool to Density_Data as well as a method for removing the extraneous revision sets

---
+++ examples/Human/src/retrieve_info_of_genes.py
@@ -0,0 +1,145 @@
+#!/usr/bin/env/python
+
+"""
+Retrieve info of a list of genes
+"""
+
+__author__ = "Scott Teresi"
+
+import argparse
+import os
+import logging
+import coloredlogs
+
+from transposon.gene_data import GeneData
+from transposon.density_data import DensityData
+from transposon.import_filtered_genes import import_filtered_genes
+
+
+def verify_h5_cache(h5_file, gene_data_instance):
+    """
+    Verify that previously value swapped H5 values are saved to disk and import
+    those instead of swapping the values once more
+
+    Args:
+        h5_file (str): Path to string of TE density HDF5 file saved to disk
+        gene_data_instance (GeneData): An instance of GeneData
+
+    Returns:
+        processed_density_data (DensityData): An instance of DensityData that
+        combines information from the TE density HDF5 data and GeneData
+    """
+    if os.path.exists(h5_file.replace(".h5", "_SenseSwapped.HDF5")):
+        logger.info("Previous sense swapped data exists, reading...")
+        processed_density_data = DensityData(
+            h5_file, gene_data_instance, logger, sense_swap=False
+        )
+    else:
+        logger.info("Writing new sense swapped DensityData...")
+        processed_density_data = DensityData(h5_file, gene_data_instance, logger)
+    return processed_density_data
+
+
+if __name__ == "__main__":
+    path_main = os.path.abspath(__file__)
+    dir_main = os.path.dirname(path_main)
+    parser = argparse.ArgumentParser(
+        description="""output to std_out information of each gene in a
+        user-provided list of genes using the DensityData.info_of_gene()
+        function"""
+    )
+
+    parser.add_argument(
+        "te_density_hdf5_result",
+        type=str,
+        help="""Path to the HDF5 file that contains the genes that the user wants
+        to extract TE density data from""",
+    )
+
+    parser.add_argument(
+        "cleaned_gene_data_file",
+        type=str,
+        help="""Path to the cleaned gene data
+        file that was produced prior to running the pipeline, this is necessary
+        to initialize the DensityData obj""",
+    )
+
+    parser.add_argument(
+        "chromosome_to_subset_gene_data",
+        type=str,
+        help="""The cleaned gene data
+        file may contain information of genes from multiple chromosomes,
+        because the TE Density data corresponds to one chromosome, please
+        specify the appropriate chromosome identifier for the density data you
+        are trying to access, so that we may appropriately subset the gene
+        data""",
+    )
+
+    parser.add_argument(
+        "genome_id",
+        type=str,
+        help="""Please specify the genome ID, use the same one you used as an
+        argument when running TE Density""",
+    )
+
+    parser.add_argument(
+        "list_of_genes",
+        type=str,
+        help="Path to list of genes files",
+    )
+
+    parser.add_argument(
+        "--window_idx",
+        type=int,
+        default=1,
+        help="""Index of the window that you want to access information from,
+        windows go from lowest to highest, default TE density settings yield 20
+        windows (500, 10000, 500) (start, stop, step).""",
+    )
+
+    parser.add_argument(
+        "--n_te_types",
+        type=int,
+        default=5,
+        help="""Number of TE types that you want to display when showing the
+        top and bottom TE categories for density relative to your gene of
+        interest""",
+    )
+
+    parser.add_argument(
+        "-v", "--verbose", action="store_true", help="set debugging level to DEBUG"
+    )
+
+    args = parser.parse_args()
+    args.te_density_hdf5_result = os.path.abspath(args.te_density_hdf5_result)
+    args.cleaned_gene_data_file = os.path.abspath(args.cleaned_gene_data_file)
+    args.list_of_genes = os.path.abspath(args.list_of_genes)
+    log_level = logging.DEBUG if args.verbose else logging.INFO
+    logger = logging.getLogger(__name__)
+    coloredlogs.install(level=log_level)
+
+    # Read pandas dataframe from cleaned genes file
+    full_genome_gene_data = import_filtered_genes(args.cleaned_gene_data_file, logger)
+
+    # MAGIC, chromosome category is inherent to the pandas data frame
+    full_genome_gene_data_subsetted = full_genome_gene_data.loc[
+        full_genome_gene_data["Chromosome"] == args.chromosome_to_subset_gene_data
+    ]
+
+    # Initialize GeneData obj
+    specific_chromosome_gene_data = GeneData(
+        full_genome_gene_data_subsetted, args.genome_id
+    )
+
+    # Initialize DensityData obj
+    processed_density_data = verify_h5_cache(
+        args.te_density_hdf5_result, specific_chromosome_gene_data
+    )
+
+    # Read list of genes file from user and begin printing output to console
+    with open(args.list_of_genes, "r", encoding="utf-8") as in_file:
+        all_genes = [gene.strip() for gene in in_file]
+    for gene in all_genes:
+        print(
+            processed_density_data.info_of_gene(gene, args.window_idx, args.n_te_types)
+        )
--- transposon/density_data.py
+++ transposon/density_data.py
@@ -24,10 +24,14 @@ class DensityData:
         gene_data (GeneData):
         """
         new_filename = input_h5.replace(".h5", "_SenseSwapped.HDF5")
-        if not os.path.isfile(new_filename):
-            shutil.copyfile(input_h5, new_filename)  # copy because we
-            # need to swap values
+        # if not os.path.isfile(new_filename):
+        shutil.copyfile(input_h5, new_filename)  # copy because we
+        # need to swap values later
         self.data_frame = h5py.File(new_filename, "r+")
+
+        # Remove the revision groupings from the dataset as they are an
+        # artifact from accurately merging TEs
+        self.data_frame = self.remove_revision_sets()
 
         self.key_list = list(self.data_frame.keys())
         self.gene_list = self.data_frame["GENE_NAMES"][:]
@@ -38,6 +42,7 @@ class DensityData:
             raise ValueError(
                 "There are multiple unique chromosomes in this density data."
             )
+
         self.unique_chromosome_id = self.unique_chromosomes[0]  # MAGIC
         # self.name = self.chromosomes.unique()
         self.windows = self.data_frame["WINDOWS"]  # not int
@@ -65,6 +70,58 @@ class DensityData:
 
         if sense_swap:
             self._swap_strand_vals(genes_to_swap)
+
+        # TODO
+        # Do i need to call self.data_frame.close() here?
+
+    def remove_revision_sets(self):
+        for te_order_idx, te_order_name in enumerate(self.data_frame["ORDER_NAMES"][:]):
+            if te_order_name == "S_Revision":
+                order_idx_to_del = te_order_idx
+                order_names_less_revision = np.delete(
+                    self.data_frame["ORDER_NAMES"], [order_idx_to_del]
+                )
+                del self.data_frame["ORDER_NAMES"]
+                self.data_frame.create_dataset(
+                    "ORDER_NAMES", data=order_names_less_revision
+                )
+
+        for te_super_idx, te_super_name in enumerate(
+            self.data_frame["SUPERFAMILY_NAMES"][:]
+        ):
+            if te_super_name == "O_Revision":
+                super_idx_to_del = te_super_idx
+                super_names_less_revision = np.delete(
+                    self.data_frame["SUPERFAMILY_NAMES"], [super_idx_to_del]
+                )
+                del self.data_frame["SUPERFAMILY_NAMES"]
+                self.data_frame.create_dataset(
+                    "SUPERFAMILY_NAMES", data=super_names_less_revision
+                )
+
+        #
+        order_datasets = ["RHO_ORDERS_LEFT", "RHO_ORDERS_INTRA", "RHO_ORDERS_RIGHT"]
+        super_datasets = [
+            "RHO_SUPERFAMILIES_LEFT",
+            "RHO_SUPERFAMILIES_INTRA",
+            "RHO_SUPERFAMILIES_RIGHT",
+        ]
+
+        for order_dataset in order_datasets:
+            data_less_revision = np.delete(
+                self.data_frame[order_dataset], order_idx_to_del, 0
+            )
+            del self.data_frame[order_dataset]
+            self.data_frame.create_dataset(order_dataset, data=data_less_revision)
+
+        for super_dataset in super_datasets:
+            data_less_revision = np.delete(
+                self.data_frame[super_dataset], super_idx_to_del, 0
+            )
+            del self.data_frame[super_dataset]
+            self.data_frame.create_dataset(super_dataset, data=data_less_revision)
+
+        return self.data_frame
 
     def _index_of_gene(self, gene_string):
         """Return the index of a gene given the name of the gene
@@ -179,6 +236,126 @@ class DensityData:
             self.data_frame["RHO_ORDERS_RIGHT"][:, :, index_to_switch] = left_val_order
 
             self.data_frame["RHO_ORDERS_LEFT"][:, :, index_to_switch] = right_val_order
+
+    def info_of_gene(self, gene_id, window_idx, n_te_types=5):
+        """
+        gene_id (str): String representing the name of the gene to report
+        information
+
+        window_idx (int): Integer representing the index of the window that you
+        want to display information for, the smallest window starts at 0.
+
+        n_te_types (int): Defaults to 5, integer representing how many TE types
+        to show for the greatest and least values.
+        """
+        gene_index = self._index_of_gene(gene_id)
+        window_val = self.window_list[window_idx]
+
+        # TODO candidate to clean up below, utility function for those not
+        # well-versed in indexing HDF5
+
+        # NB sort in reverse order, the last items in this array contains
+        # the greatest density value
+        # ORDERS
+        sorted_order_left_indices = np.argsort(
+            self.left_orders[:, window_idx, gene_index]
+        )
+        sorted_order_intronic_indices = np.argsort(self.intra_orders[:, 0, gene_index])
+        sorted_order_right_indices = np.argsort(
+            self.right_orders[:, window_idx, gene_index]
+        )
+
+        # SUPERFAMILIES
+        sorted_super_left_indices = np.argsort(
+            self.left_supers[:, window_idx, gene_index]
+        )
+        sorted_super_intronic_indices = np.argsort(self.intra_supers[:, 0, gene_index])
+        sorted_super_right_indices = np.argsort(
+            self.right_supers[:, window_idx, gene_index]
+        )
+
+        # NB sorted array, containing actual values now
+        # ORDERS
+        sorted_order_left = self.left_orders[:, window_idx, gene_index][
+            sorted_order_left_indices
+        ]
+        sorted_order_intra = self.intra_orders[:, 0, gene_index][
+            sorted_order_intronic_indices
+        ]
+        sorted_order_right = self.right_orders[:, window_idx, gene_index][
+            sorted_order_right_indices
+        ]
+        # SUPERFAMILIES
+        sorted_super_left = self.left_supers[:, window_idx, gene_index][
+            sorted_super_left_indices
+        ]
+        sorted_super_intra = self.intra_supers[:, 0, gene_index][
+            sorted_super_intronic_indices
+        ]
+        sorted_super_right = self.right_supers[:, window_idx, gene_index][
+            sorted_super_right_indices
+        ]
+
+        info = f"""
+        -------------------------------------------------
+        Gene Index and ID: {gene_index, gene_id}
+        Window Index and Value: {window_idx, window_val}
+        Chosen N TE Types to Display: {n_te_types}
+        TE Orders in Annotation: {self.order_list}
+        TE SuperFamilies in Annotation: {self.super_list}
+        NOTE, there can be multiple entries with 0 as a density value, so if
+        zero are returned, there very well might be more.
+        -------------------------------------------------
+
+        TOP {n_te_types} TE ORDERS:
+            Upstream:
+                {np.array(self.order_list)[sorted_order_left_indices[-n_te_types:]]}
+                {sorted_order_left[-n_te_types:]}
+            Intronic:
+                {np.array(self.order_list)[sorted_order_intronic_indices[-n_te_types:]]}
+                {sorted_order_intra[-n_te_types:]}
+            Downstream:
+                {np.array(self.order_list)[sorted_order_right_indices[-n_te_types:]]}
+                {sorted_order_right[-n_te_types:]}
+
+        BOTTOM {n_te_types} TE ORDERS:
+            Upstream:
+                {np.array(self.order_list)[sorted_order_left_indices[:n_te_types]]}
+                {sorted_order_left[:n_te_types]}
+            Intronic:
+                {np.array(self.order_list)[sorted_order_intronic_indices[:n_te_types]]}
+                {sorted_order_intra[:n_te_types]}
+            Downstream:
+                {np.array(self.order_list)[sorted_order_right_indices[:n_te_types]]}
+                {sorted_order_right[:n_te_types]}
+
+        -------------------------------------------------
+
+        TOP {n_te_types} SUPERFAMILIES:
+            Upstream:
+                {np.array(self.super_list)[sorted_super_left_indices[-n_te_types:]]}
+                {sorted_super_left[-n_te_types:]}
+            Intronic:
+                {np.array(self.super_list)[sorted_super_intronic_indices[-n_te_types:]]}
+                {sorted_super_intra[-n_te_types:]}
+            Downstream:
+                {np.array(self.super_list)[sorted_super_right_indices[-n_te_types:]]}
+                {sorted_super_right[-n_te_types:]}
+
+        BOTTOM {n_te_types} SUPERFAMILIES:
+            Upstream:
+                {np.array(self.super_list)[sorted_super_left_indices[:n_te_types]]}
+                {sorted_super_left[:n_te_types]}
+            Intronic:
+                {np.array(self.super_list)[sorted_super_intronic_indices[:n_te_types]]}
+                {sorted_super_intra[:n_te_types]}
+            Downstream:
+                {np.array(self.super_list)[sorted_super_right_indices[:n_te_types]]}
+                {sorted_super_right[:n_te_types]}
+
+        -------------------------------------------------
+        """
+        return info
 
     def __repr__(self):
         """String representation for developer."""
